72f8577fbaf27d7079bf55b7b27b9276ca8ac2de
Iqbalfachrian <iqbalfachrian@gmail.com>
2026-08-27 16:54:06 +0700

    feat: add reports.spec.ts

---
+++ tests/orangeHRM/PIM/reports.spec.ts
@@ -0,0 +1,74 @@
+import { test, expect } from '@playwright/test'
+import { loginOrangeHRM } from '../helper/auth.helper'
+import path from 'path'
+
+test.describe('Navigate to PIM Menu', () => {
+    test.beforeEach(async ({ page }) => {
+        await loginOrangeHRM(page);
+
+        await page.getByRole('link', { name: 'PIM'}).click();
+        await expect(page).toHaveURL(/viewEmployeeList/);
+
+        await page.getByRole('link', {name: 'Reports'}).click();
+        await expect(page).toHaveURL(/viewDefinedPredefinedReports/)
+    } )
+
+    test('Test-001: Go to Reports and do search', async ({ page }) => {
+        
+        const reportName = page.getByPlaceholder('Type for Hints...');
+        await expect(reportName).toBeVisible();
+        await expect(reportName).toBeEditable();
+        await reportName.fill('Employee Job Details');
+
+        const suggestion = page
+        .locator('div.oxd-autocomplete-wrapper')
+        .filter({ hasText: 'Employee Job Details'});
+
+        await expect(suggestion).toBeVisible({ timeout: 3000})
+        await suggestion.click()
+        await expect(reportName).toHaveValue('Employee Job Details');
+
+        await page.getByRole('button', { name: 'Search'}).click();
+        await expect(page.getByText('Employee Job Details')).toBeVisible();
+    })
+
+    test('Test-002: Add Employee Reports', async ({ page }) => {
+        await page.getByRole('button', { name: 'Add'}).click();
+        await expect(page).toHaveURL(/definePredefinedReport/);
+        await expect(page.getByText('Add Report')).toBeVisible();
+
+        await page.getByRole('textbox', {name:'Type here ...'}).fill('Anang');
+
+        const selectionCriteria = page
+        .locator('.oxd-input-group')
+        .filter({
+            has: page.locator('label', {hasText: 'Selection Criteria'})
+        })
+
+        const pickCriteria = selectionCriteria.locator('.oxd-select-wrapper')
+        await pickCriteria.click();
+        await page
+        .getByRole('listbox')
+        .getByText('Employee Name', {exact: true})
+        .click();
+
+        const plusButton = page
+        .locator('button:has(i.bi-plus)').first();
+        await plusButton.click();
+
+        await page.locator('button:has(i.bi-plus)').first().click();
+
+        //Fill autocomplete
+        const employeeName = page.getByPlaceholder('Type for hints...')
+
+        await expect(employeeName).toBeVisible();
+        await expect(employeeName).toBeEditable();
+        await employeeName.fill('Timothy')
+
+        const targetOption = page.getByRole('option', { name: 'Timothy Lewis Amiano'})
+        await expect(targetOption).toBeVisible({ timeout: 5000})
+        await targetOption.click();
+        await expect(employeeName).toHaveValue('Timothy Lewis Amiano')
+    
+    })
+})
